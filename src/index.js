50b7fced705ffe97609fb1a06fbb323f425beaa4
YuniorGlez <yunior@squaads.com>
2024-12-09 18:58:55 +0000

    Simplemente, recogo los valores de los inputs y hago una validacion del nombre y de momento true de email y de edad. Solo en caso de no ser invalido alguno lo meto en el array

--- src/index.js
+++ src/index.js
@@ -4,8 +4,43 @@ const age = document.getElementById("age");
 const addButton = document.getElementById("add-button");
 const usersList = document.getElementById("users-list");
 
+const users = [];
+
 addButton.addEventListener("click", addUser);
 
 function addUser() {
-  alert("Detecto el click en añadir");
+  const nameValue = name.value.trim();
+  const emailValue = email.value.trim().toLowerCase();
+  const ageValue = parseInt(age.value);
+
+  if (!validateName(nameValue)) {
+    return;
+  }
+  if (!validateEmail(emailValue)) {
+    return;
+  }
+  if (!validateAge(ageValue)) {
+    return;
+  }
+
+  const user = {
+    name: nameValue,
+    email: emailValue,
+    age: ageValue,
+  };
+
+  users.push(user);
+
+  console.log(`Tenemos ${users.length} usuarios`);
+}
+
+function validateName(name) {
+  return (name && name.length >= 3);
+}
+
+function validateEmail(email) {
+  return true;
+}
+function validateAge(age) {
+  return true;
 }
